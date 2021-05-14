b8aa0d29b6b53ab67dc6c8a72f4c4cb84344c134
meghanabhat29 <meghanaleo29@gmail.com>
2021-05-15 01:01:03 +0530

    Added colour palette for home page

---
+++ code/metro_run/lib/color_collection.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorCollection {
+  final Color titleColor = Colors.green[800];
+  final Color firstSlice = Color(0xFF57CFEA);
+  final Color secondSlice = Color(0xFFFCC07E);
+  final Color buttonColor = Color(0xFFFD866E);
+  final Color leftBarColor = Colors.green[100];
+}
